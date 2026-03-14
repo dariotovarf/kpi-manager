ae3f67fa4539405c77e3c615332e5734e570f0a8
dariotovarf <hermendtf@gmail.com>
2026-03-14 18:50:48 -0500

    despliegue

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/kpi_manager"
+##DA ABASE_URL = "postgresql://postgres:postgres@localhost:5432/kpi_manager"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
